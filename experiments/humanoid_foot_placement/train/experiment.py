3c88ad67b70950cc0d36383a4f97cd18e4611571
Alessandro Montenegro <73550926+MontenegroAlessandro@users.noreply.github.com>
2025-12-20 11:22:18 +0100

    cluster disabled video

--- experiments/humanoid_foot_placement/train/experiment.py
+++ experiments/humanoid_foot_placement/train/experiment.py
@@ -149,10 +149,11 @@ def experiment(config: DictConfig):
         # print(f"Time taken to log metrics: {time.time() - t_start}s")
 
         # run the environment with the trained agent to record video
-        
-        PPOJax.play_policy(env, agent_conf, agent_state, deterministic=True, n_steps=1000, n_envs=1, record=True, train_state_seed=0)
-        video_file = env.video_file_path
-        run.log({"Agent Video": wandb.Video(video_file)})
+        rec = os.getenv("RECORD_POLICY")
+        if rec is None or rec:
+            PPOJax.play_policy(env, agent_conf, agent_state, deterministic=True, n_steps=1000, n_envs=1, record=True, train_state_seed=0)
+            video_file = env.video_file_path
+            run.log({"Agent Video": wandb.Video(video_file)})
 
         wandb.finish()
 
